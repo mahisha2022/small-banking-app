54405ab3339482f962397a61d253c16b4a1710d3
alex <ackerander@gmail.com>
2023-03-08 22:16:40 -0800

    Fixed tables not droping bug.

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -2,9 +2,9 @@
 --first, drop your tables (to reset your database for testing)
 --then create your tables
 
-drop table if exists bank_user;
-drop table if exists account;
 drop table if exists transactions;
+drop table if exists account;
+drop table if exists bank_user;
 
 create table bank_user (
     user_id int primary key auto_increment,
