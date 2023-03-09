c5eb9aa6478759e3cb947cee9d03dca43ae38c63
alex <ackerander@gmail.com>
2023-03-08 23:10:04 -0800

    Added login endpoint tests.

--- src/test/java/BankTest.java
+++ src/test/java/BankTest.java
@@ -41,20 +41,20 @@ public class BankTest {
 	}
 
 	/**
-	* Valid user (username = "user", password = "password") should respond 200 with user as body.
+	* POST to localhost:8080/register
+	* Valid new user (username = "user", password = "password") should respond 200 with user as body.
 	*/
 	@Test
 	public void registerTest() throws IOException, InterruptedException {
 		HttpRequest postRequest = HttpRequest.newBuilder()
 			.uri(URI.create("http://localhost:8080/register"))
 			.POST(HttpRequest.BodyPublishers.ofString(
-				"{\"username\": \"user\", \"password\": \"password\" }"
+				"{\"username\": \"user\", \"password\": \"password\"}"
 			)).header("Content-Type", "application/json").build();
 		HttpResponse response = webClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
 		Assert.assertEquals(200, response.statusCode());
-		BankUser expectedUser = new BankUser("user", "password");
+		BankUser expectedUser = new BankUser(1, "user", "password");
 		BankUser actualUser = mapper.readValue(response.body().toString(), BankUser.class);
-		actualUser.setUser_id(expectedUser.getUser_id());
 		Assert.assertEquals(expectedUser, actualUser);
 	}
 
@@ -66,7 +66,7 @@ public class BankTest {
 		HttpRequest postRequest = HttpRequest.newBuilder()
 			.uri(URI.create("http://localhost:8080/register"))
 			.POST(HttpRequest.BodyPublishers.ofString(
-				"{\"username\": \"\", \"password\": \"password\" }"
+				"{\"username\": \"\", \"password\": \"password\"}"
 			)).header("Content-Type", "application/json").build();
 		HttpResponse response = webClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
 		Assert.assertEquals(400, response.statusCode());
@@ -80,7 +80,7 @@ public class BankTest {
 		HttpRequest postRequest = HttpRequest.newBuilder()
 			.uri(URI.create("http://localhost:8080/register"))
 			.POST(HttpRequest.BodyPublishers.ofString(
-				"{\"username\": \"user\", \"password\": \"p\" }"
+				"{\"username\": \"user\", \"password\": \"p\"}"
 			)).header("Content-Type", "application/json").build();
 		HttpResponse response = webClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
 		Assert.assertEquals(400, response.statusCode());
@@ -95,9 +95,59 @@ public class BankTest {
 		HttpRequest postRequest = HttpRequest.newBuilder()
 			.uri(URI.create("http://localhost:8080/register"))
 			.POST(HttpRequest.BodyPublishers.ofString(
-				"{\"username\": \"\", \"password\": \"password\" }"
+				"{\"username\": \"\", \"password\": \"password\"}"
 			)).header("Content-Type", "application/json").build();
 		HttpResponse response = webClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
 		Assert.assertEquals(400, response.statusCode());
+	}
+
+	/**
+	* POST to localhost:8080/login
+	* Valid existing user (register user first) should respond 200 with user as body.
+	*/
+	@Test
+	public void loginTest() throws IOException, InterruptedException {
+		registerTest();
+		HttpRequest postLoginRequest = HttpRequest.newBuilder()
+			.uri(URI.create("http://localhost:8080/login"))
+			.POST(HttpRequest.BodyPublishers.ofString(
+			"{\"username\": \"user\", \"password\": \"password\"}"
+			)).header("Content-Type", "application/json")
+			.build();
+		HttpResponse loginResponse = webClient.send(postLoginRequest, HttpResponse.BodyHandlers.ofString());
+		Assert.assertEquals(200, loginResponse.statusCode());
+		BankUser expectedUser = new BankUser(1, "user", "password");
+		Assert.assertEquals(expectedUser, mapper.readValue(loginResponse.body().toString(), BankUser.class));
+	}
+	
+	/**
+	* Login to nonexisting account should fail (respond 401).
+	*/
+	@Test
+	public void loginNonexistTest() throws IOException, InterruptedException {
+		HttpRequest postRequest = HttpRequest.newBuilder()
+			.uri(URI.create("http://localhost:8080/login"))
+			.POST(HttpRequest.BodyPublishers.ofString(
+				"{\"username\": \"user\", \"password\": \"password\"}"
+			)).header("Content-Type", "application/json")
+			.build();
+		HttpResponse response = webClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+		Assert.assertEquals(401, response.statusCode());
+	}
+
+	/**
+	* Login attempt with incorrect password should fail (respond 401).
+	*/
+	@Test
+	public void loginIncorrecPasswordTest() throws IOException, InterruptedException {
+		registerTest();
+		HttpRequest postLoginRequest = HttpRequest.newBuilder()
+		.uri(URI.create("http://localhost:8080/login"))
+		.POST(HttpRequest.BodyPublishers.ofString(
+			"{\"username\": \"user\", \"password\": \"wrongpassword\"}"
+		)).header("Content-Type", "application/json")
+		.build();
+		HttpResponse loginResponse = webClient.send(postLoginRequest, HttpResponse.BodyHandlers.ofString());
+		Assert.assertEquals(401, loginResponse.statusCode());
 	}
 }
